e1322a379d8aa95335bf36f1853d3101077ec9ca
NGUYỄN THANH SƠN <nguyenthanhson162001@gmail.com>
2022-04-19 10:23:35 +0700

    add security helmet and limiter and edit url in fontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { rateLimit } from 'express-rate-limit'
+import helmet from 'helmet'
 import cors from 'cors'
 import 'dotenv/config'
 import { connectDB } from './config/database/mongoDB'
@@ -8,12 +10,27 @@ import https from 'https'
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 // connect DB mongoDB
 connectDB();
 const options = {
     key: fs.readFileSync('ssl/key.pem'),
     cert: fs.readFileSync('ssl/cert.pem')
 };
+
+// security DOS, DDOS
+const limiter = rateLimit({
+    // 15 minutes
+    windowMs: 15 * 60 * 1000,
+    // limit each IP to 100 requests per windowMs
+    max: 100
+});
+app.use(limiter);
+
+// security filter request toxic
+app.use(helmet())
+
+
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
